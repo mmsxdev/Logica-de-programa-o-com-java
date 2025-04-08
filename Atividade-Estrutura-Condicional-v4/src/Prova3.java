ce3c0a0afc1dc16a1524bc6249eebe57e40caf95
Miguel Melo <hjumiguel@gmail.com>
2025-04-07 21:35:45 -0300

    Update Prova3.java

--- Atividade-Estrutura-Condicional-v4/src/Prova3.java
+++ Atividade-Estrutura-Condicional-v4/src/Prova3.java
@@ -1,27 +1,27 @@
-import java.nio.ByteBuffer;
 import java.util.Scanner;
 
-public class Prova3{
+public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        double A,B,C;
-        A = input.nextDouble();
-        B = input.nextDouble();
-        C = input.nextDouble();
+        Scanner sc = new Scanner(System.in);
 
-        double delta = B*2 - 4 * A * C;
+        // Lê os três valores A, B e C
+        double A = sc.nextDouble();
+        double B = sc.nextDouble();
+        double C = sc.nextDouble();
 
-        double x1 = ( -B + Math.sqrt(delta)) / 2*A;
-        double x2 = ( -B - Math.sqrt(delta)) / 2*A;
+        // Calcula o delta
+        double delta = B * B - 4 * A * C;
 
-        if (A == 0|| B == 0 || C == 0){
+        // Verifica se é possível calcular
+        if (A == 0 || delta < 0) {
             System.out.println("Impossivel calcular");
+        } else {
+            double R1 = (-B + Math.sqrt(delta)) / (2 * A);
+            double R2 = (-B - Math.sqrt(delta)) / (2 * A);
+            System.out.printf("R1 = %.5f%n", R1);
+            System.out.printf("R2 = %.5f%n", R2);
         }
-        else if (B < A){
-            System.out.println("Impossivel calcular");
-        }
-        else {
-            System.out.println("(\"R1 = \" + x1 + \"\\nR2 = \" + x2);");
-        }
+
+        sc.close();
     }
 }
