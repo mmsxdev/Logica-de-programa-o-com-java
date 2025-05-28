623228d73e9f72d96c18c51c1eaf0db1c3275c8a
mmsxdev <hjumiguel@gmail.com>
2025-05-27 21:43:15 -0300

    add questões becrowd 6 exercicios - exercicio 2

--- Estrutura-de-Dados-Vetor/src/Becrowd26_05_2025/Atividade2.java
+++ Estrutura-de-Dados-Vetor/src/Becrowd26_05_2025/Atividade2.java
@@ -1,7 +1,20 @@
 package Becrowd26_05_2025;
 
+import java.util.Scanner;
+
 public class Atividade2 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int N[] = new int[10];
+        int x;
+
+        x = sc.nextInt();
+
+        for (int i = 0; i < N.length; i++) {
+            N[i] = x;
+            x = x * 2;
+            System.out.printf("N[%d] = %d\n", i, N[i]);
+        }
     }
 }
